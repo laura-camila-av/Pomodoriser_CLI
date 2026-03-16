ee78a67165189feb3146d03fe27aafbb9aeb463d
Laura <23802255@student.uwa.edu.au>
2026-03-16 18:01:51 +0800

    called append task function:

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #Opening page
 
 #Eventually could break down insert attribute so tasks are added once at a time.
+import array as arr
+
 if __name__ == "__main__":
     import taskPopulator
     import instructions
     
+
     instructions.showInstructions()
 
     taskName = taskPopulator.requestTaskName()
     pomodoroNo = taskPopulator.requestTaskTime()
     taskPriority = taskPopulator.requestTaskPriority()
-    taskPopulator.insertAtribute(taskName, pomodoroNo, taskPriority)
-
+    new_task = taskPopulator.insertAtribute(taskName, pomodoroNo, taskPriority)
+    taskPopulator.appendTask(new_task)
 
 
     
