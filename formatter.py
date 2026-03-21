f609e4ccfe894ba76449eb00c61109d3001dd51a
Laura <23802255@student.uwa.edu.au>
2026-03-21 13:58:09 +0800

    Confirming everything is up to date

--- formatter.py
+++ formatter.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 def planHeader():
     return(
     """
@@ -12,14 +13,29 @@ def taskDivider():
     +-------|+------------------------+ 
     """
     )
+def getStartTime():
+    startTime = input("Enter the time in HH:MM format:")
+    return datetime.strptime(startTime, "%H:%M")
+    startTime = getStartTime()
 
-def fillTable(sortedPlans):
-    print("+-----+----------------------------------------------------+")
-    print("| {:<3} | {:<50} |".format("No.", "Task"))
-    print("+-----+----------------------------------------------------+")
+    #error catcher to come in future
+
+def createTimesList(startTime, sortedPlans):
+    times = []
+    current_time = startTime
+    for t in range(len(sortedPlans)):
+        times += [current_time]
+        current_time += timedelta(minutes=30)
+    return times
+
+
+def fillTable(sortedPlans, times):
+    print("+-----+------------+----------------------------------------------------+")
+    print("| {:<3} | {:<10} | {:<50} |".format("No.", "Time", "Task"))
+    print("+-----+------------+----------------------------------------------------+")
     for x in range(len(sortedPlans)):
-        print("| {:<3} | {:<50} |".format(sortedPlans[x][0], sortedPlans[x][1]))
-        print("+-----+----------------------------------------------------+")
+        print("| {:<3} | {:<10} | {:<50} |".format(sortedPlans[x][0], times[x].strftime("%H:%M"), sortedPlans[x][1]))
+        print("+-----+------------+----------------------------------------------------+")
 
     
       
