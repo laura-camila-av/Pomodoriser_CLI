8933e5ab85b1d148d70b7d958935b1d8b159e158
Laura <23802255@student.uwa.edu.au>
2026-03-21 13:58:29 +0800

    Confirming everything is up to date

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-#Opening page
 
-#Eventually could break down insert attribute so tasks are added once at a time.
+
 import array as arr
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     import taskPopulator
     import instructions
     import formatter
+    from datetime import datetime, timedelta
     
 
     instructions.showInstructions()
@@ -26,17 +27,12 @@ if __name__ == "__main__":
             Adding = False
     print("These are the raw unfiltered plans. In later versions this will be hidden and not printed until it is formatted as a table.")
     sortedPlans = taskPopulator.sortByPriority(unsortedPlans)
+
     taskPopulator.assignRanks(sortedPlans)
     print(sortedPlans)
     #header = formatter.planHeader()
     #print(header)
-    formatted_plan = formatter.fillTable(sortedPlans)
+    startTime = formatter.getStartTime()
+    times = formatter.createTimesList(startTime, sortedPlans)
+    formatted_plan = formatter.fillTable(sortedPlans, times)
     print(formatted_plan)
-
-
-
-    #sortedPlan = taskPopulator.sortByPriority(unsortedPlans)
-   #1
-   
-
-    
