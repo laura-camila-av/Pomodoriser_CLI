09a2834fd3962222eb072768a439b70385364cc8
Laura <23802255@student.uwa.edu.au>
2026-03-22 19:28:21 +0800

    Changed structure. plan ends when time runs out rather than relying user to enter c or g

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ if __name__ == "__main__":
     
 
     instructions.showInstructions()
+    timeAvailable = taskPopulator.getPlanLength()
     unsortedPlans = []
     Adding = True
 
-    while Adding:
+    rowCount = int(0)
+
+    while rowCount < timeAvailable: #should change this conditional to depend on amount time of time left?
         taskName = taskPopulator.requestTaskName()
         pomodoroNo = taskPopulator.requestTaskTime()
+        if (int(pomodoroNo) + rowCount) >= timeAvailable:
+            print("Adding this plan will take you to the end of your time limit.\nTasks exceeding the time limit will not be included in the plan.")
         taskPriority = taskPopulator.requestTaskPriority()
         new_task = taskPopulator.insertAtribute(taskName, pomodoroNo, taskPriority)
         taskPopulator.appendTask(unsortedPlans, new_task)
-        userIsDone = str(input("To add another tasks, press enter 'C'.\nTo stop adding tasks and generate the plan, enter 'G'."))
-        if userIsDone == 'c' or userIsDone == 'C':
-            Adding = True
-        else:
-            Adding = False
-    print("These are the raw unfiltered plans. In later versions this will be hidden and not printed until it is formatted as a table.")
+        #userIsDone = str(input("To add another tasks, press enter 'C'.\nTo stop adding tasks and generate the plan, enter 'G'."))
+  
+            #This will debugged out in future versions when the program goes beyond the command line.
+        rowCount += int(pomodoroNo)
+
+    print("You have filled all the available time. Your plan will now be generated.")
+
     sortedPlans = taskPopulator.sortByPriority(unsortedPlans)
 
     taskPopulator.assignRanks(sortedPlans)
-    print(sortedPlans)
-    #header = formatter.planHeader()
-    #print(header)
+  
     startTime = formatter.getStartTime()
     times = formatter.createTimesList(startTime, sortedPlans)
     formatted_plan = formatter.fillTable(sortedPlans, times)
